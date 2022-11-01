a6da81cffd890e6d5d9b7a03ec905565ea9c727d
godkate <bogodul883@gmail.com>
2022-10-31 22:42:35 -0400

    Added script

--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@
  * @param {number} maxSecond
  * @returns {number}
  */
-export const minMaxMultiply = (minFirst, minSecond, maxFirst, maxSecond) => {};
+export const minMaxMultiply = (minFirst, minSecond, maxFirst, maxSecond) => {
+  let result = (Math.min(minFirst, minSecond)*(Math.max(maxFirst, maxSecond)));
+  return result;
+};
 
 /**
  * Функция принимает один числовой аргумент, это радиус круга. Функция должна
@@ -16,7 +19,10 @@ export const minMaxMultiply = (minFirst, minSecond, maxFirst, maxSecond) => {};
  * @param {number} radius
  * @returns {number}
  */
-export const getSquareOfCircle = (radius) => {};
+export const getSquareOfCircle = (radius) => {
+  let result = Math.PI * radius ** 2;
+  return result;
+};
 
 /**
  * Функция принимает число и точность к которой нужно округлить
@@ -26,7 +32,11 @@ export const getSquareOfCircle = (radius) => {};
  * @param {number} accuracy
  * @returns {number}
  */
-export const getFixedValue = (number, accuracy) => {};
+export const getFixedValue = (number, accuracy) => {
+  let quantityAfterPoint = 10 ** accuracy;
+  let result = Math.round(number * quantityAfterPoint) / quantityAfterPoint;
+  return result;
+};
 
 /**
  * Если целая часть числа четная, то срабатывает Math.floor для этого числа
@@ -34,7 +44,14 @@ export const getFixedValue = (number, accuracy) => {};
  * @param {number} number
  * @returns {number}
  */
-export const getFlooroCeil = (number) => {};
+export const getFlooroCeil = (number) => {
+  let result = 0;
+  if (Math.trunc(number) % 2 === 0) {
+    return result = Math.floor(number);
+  } else {
+    return result = Math.ceil(number);
+  }
+};
 
 /**
  * Мы передаем два числа, от какого и до какого числа и функция должна
@@ -44,4 +61,7 @@ export const getFlooroCeil = (number) => {};
  * @param {number} to
  * @returns {number}
  */
-export const randomizer = (from, to) => {};
+export const randomizer = (from, to) => {
+  let result = Math.round(from + Math.random() * (to - from));
+  return result;
+};
